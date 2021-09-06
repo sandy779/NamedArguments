7e5461c1ae4a1433a18f3420acd1edf949217134
sandip ahire <sandipahire34@gmail.com>
2021-09-06 14:17:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NamedArguments
+{
+    class Product
+    {
+        public void PrintOrderDetails(object ordernum, string productname, string sellername)
+        {
+            Console.WriteLine("Order details are:\nOrderno:{0} Productname:{1} Sellername:{2}", ordernum, productname, sellername);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Product obj = new Product();
+            obj.PrintOrderDetails(ordernum:20 , productname: "Stationary", sellername: "Sandip");
+        }
+    }
+}
